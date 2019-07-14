346c2dc3a1a77782a5f659b0a0f5669fb07cf268
micah <1013820246@qq.com>
2019-07-14 17:58:40 +0800

    第三次提交

--- src/gitDemo/Demo1.java
+++ src/gitDemo/Demo1.java
@@ -9,9 +9,11 @@ public class Demo1 {
         System.out.println("hello git");
         System.out.println("hello git");
         // 第一次修改提交
+        // 第二次修改提交
     }
     public void test01(){
         System.out.println("修改测试数据1");
         System.out.println("克隆代码后第一次测试");
+        System.out.println("克隆代码后第二次测试");
     }
 }
